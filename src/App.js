ebc0c9c902f526f65105ecc98de1c02e95176ea2
ZMichaelaK <korn_miku@hotmail.com>
2023-12-05 10:21:59 +0000

    Nav Links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Sellers from "./Components/Sellers";
+import PropertiesToLet from "./Components/PropertiesToLet";
+import Buyers from "./Components/Buyers";
+import PropertiesForSale from "./Components/PropertiesForSale";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Buyers">Buyers</Link>
+        <Link to="/Sellers">Sellers</Link>
+        <Link to="/PropertiesForSale">For sale</Link>
+        <Link to="/PropertiesToLet">To let</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Sellers" element={<Sellers />} />
+        <Route path="/Buyers" element={<Buyers />} />
+        <Route path="/PropertiesForSale" element={<PropertiesForSale />} />
+        <Route path="/PropertiesToLet" element={<PropertiesToLet />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/PropertiesForSale.jsx
@@ -0,0 +1,5 @@
+function PropertiesForSale() {
+  return <h1>Properties For Sale</h1>;
+}
+
+export default PropertiesForSale;
