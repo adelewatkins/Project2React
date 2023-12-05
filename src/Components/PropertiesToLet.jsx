2f712739a4f8ebd6c74e2ed13fb2339f96b95c0a
2023temitope <topbeemak@yahoo.com>
2023-12-05 15:00:14 +0000

    input pages

--- src/Components/PropertiesToLet.jsx
+++ src/Components/PropertiesToLet.jsx
@@ -1,5 +1,33 @@
 function PropertiesToLet() {
-  return <h1>Properties to let</h1>;
+  return (
+    <div>
+
+    <h1>Properties To Let </h1>
+
+    <form>
+      <label htmlFor="typ" >Type</label>
+      <input id="typ" type="text"></input>
+      <label htmlFor="re" >Rent</label>
+      <input id="re" type="£"></input>
+      <label htmlFor="bdr" >Bedrooms</label>
+      <input id="bdr" type="number" min={0}></input>
+      <label htmlFor="btr">Bathrooms</label>
+      <input id="btr" type="number" min={0}></input>
+      <label htmlFor="gar" >Garden</label>
+      <input id="gar" type="text"></input>
+      <label htmlFor="add" >Address</label>
+      <input id="add" type="text"></input>
+      <label htmlFor="pcd" >Postcode</label>
+      <input id="pcd" type="text"></input>
+      <br />
+      <button type="submit">Submit</button>
+
+    </form>
+
+    <br />
+
+  </div>
+  )
 }
 
 export default PropertiesToLet;
