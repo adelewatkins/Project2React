6354ca89d55eca8f4773d349b6d61cde77334443
unknown <agustinadipaolauk@gmail.com>
2023-12-08 11:48:25 +0000

    style and cards update2

--- src/Components/Sale/PropertiesForSalePT.jsx
+++ src/Components/Sale/PropertiesForSalePT.jsx
@@ -4,17 +4,24 @@ import PropertiesForSale from './PropertiesForSale';
 function PropertiesForSalePT(props) {
     return (
 
-        
-        <div>
-            <p> {props.Type}</p>
-            <p> {props.Price}</p>
-            <p> {props.Bedrooms}</p>
-            <p> {props.Bathrooms}</p>
-            <p> {props.Garden}</p>
-            <p> {props.Address}</p>
-            <p> {props.Postcode}</p>
+        <div Card style={{ width: '10%' }}>
+            <div className="col">
+                <div className="card">
+                    <div className="card-body">
+                        <div className="card-text"></div>
+                        <div className="card-title"></div> 
+                        <p>{"Price: £" +props.Price} </p>
+                        <p> {"Type:" +props.Type}</p>
+                        <p> {"Bedrooms: "+props.Bedrooms}</p>
+                        <p> {"Bathrooms:" +props.Bathrooms}</p>
+                        <p> {"Garden:" +props.Garden}</p>
+                        <p> {"Address:" +props.Address}</p>
+                        <p> {"Postcode:" +props.Postcode}</p>
 
-        </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
     );
 }
 PropertiesForSalePT.propTypes = {
