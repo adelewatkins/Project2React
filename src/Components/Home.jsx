1d7f34c0d773dc4fbe48edf2065d32058fac8e59
agustinadipaola <agustinadipaolauk@gmail.com>
2023-12-14 13:16:21 +0000

    update pictures

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,93 +4,60 @@ import RS3 from "./Pictures/RS3.png"
 import RS4 from "./Pictures/RS4.png"
 
 function Home() {
-  return( 
-  <h1>Home</h1>,
-
-
- <div>
-
-<div id="carouselExample" className="carousel slide">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-
-    <img
-                src={RS}
-                alt="RS"
-                width="100%"
-                height="15%"
-                className="d-inline-block align-text-middle"
-              />
-
+  return (
+    <h1>Home</h1>,
+    <div>
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={RS}
+              alt="RS"
+              width="100%"
+              height="15%"
+              className="d-inline-block align-text-middle"/>
+          </div>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={RS2}
+              alt="RS2"
+              width="100%"
+              height="15%"
+              className="d-inline-block align-text-middle"/>
+          </div>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={RS3}
+              alt="RS3"
+              width="100%"
+              height="15%"
+              className="d-inline-block align-text-middle"/>
+          </div>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={RS4}
+              alt="RS4"
+              width="100%"
+              height="15%"
+              className="d-inline-block align-text-middle"/>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="false"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="false"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
-    
-    
-
-    </div>
-    <div className="carousel-inner">
-    <div className="carousel-item active">
-
-    <img
-                src={RS2}
-                alt="RS2"
-                width="100%"
-                height="15%"
-                className="d-inline-block align-text-middle"
-              />
-
-    </div>
-    
-    
-
-    </div>
-    <div className="carousel-inner">
-    <div className="carousel-item active">
-
-    <img
-                src={RS3}
-                alt="RS3"
-                width="100%"
-                height="15%"
-                className="d-inline-block align-text-middle"
-              />
-
-    </div>
-    
-    
-
-    </div>
-    <div className="carousel-inner">
-    <div className="carousel-item active">
-
-    <img
-                src={RS4}
-                alt="RS4"
-                width="100%"
-                height="15%"
-                className="d-inline-block align-text-middle"
-              />
-
-    </div>
-    
-    
-
-    </div>
-    
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-
-</div>
-  
-
   )
 }
-
-
 export default Home;
